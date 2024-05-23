a2591fb3ea1c4686ea607e8e32fe6c6e4b384a13
DaryaTrifonova <113592931+DaryaTrifonova@users.noreply.github.com>
2024-05-23 16:08:22 +0300

    adding labs to one branch

---
+++ trifonova-221-332-web-dev-laborate_4/app/config.py
@@ -0,0 +1,8 @@
+import os
+
+SECRET_KEY = b'f3aa0ebdcc174d0c29d0340243d3da2b4e9d7b63f1acc158af35fd6169f74dd8'
+
+MYSQL_USER = 'std_2440_lab4'
+MYSQL_PASSWORD = 'Thetreeoflive2005'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_DATABASE = ' std_2440_lab4'
